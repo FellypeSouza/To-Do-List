8f5123c3d9f295fbd4a09959611c96bdcdc7fcfe
FellypeSouza <fellypedev23@gmail.com>
2025-03-03 18:04:14 -0300

    criando a pagina de cadastro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { RegisterPageComponent } from './Pages/register-page/register-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: RegisterPageComponent
+    }
+];
